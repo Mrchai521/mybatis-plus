e74309a783899efbd41e6fc4ddcb4705ad00085b
cxf15364946594 <1261853718@qq.com>
2020-11-27 14:33:00 +0800

    修改实体类注释

--- src/main/java/com/cxf/entity/User.java
+++ src/main/java/com/cxf/entity/User.java
@@ -14,10 +14,16 @@ public class User {
      * 用户id
      */
     private Long id;
-    //用户姓名
+    /**
+     * 用户姓名
+     */
     private String name;
-    //用户年龄
+    /**
+     * 用户年龄
+     */
     private int age;
-    //用户邮箱
+    /**
+     * 用户邮箱
+     */
     private String email;
 }
